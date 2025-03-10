1a9705634c570e481d2d72f088f5438233922952
Thanushree V <167441526+thanushree267@users.noreply.github.com>
2025-03-10 19:06:36 +0530

    I've cleaned up the comments, fixed spacing issues, and ensured clarity in your print statements.

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,6 @@
-# master
-# This program prints Hello, world!
-
 import time
 
-print("Hello I'm Geek! Let's Execute Your Code!")
+print("Hello, I'm Geek! Let's Execute Your Code!")
 time.sleep(1)
 print("Starting Our Code!")
 time.sleep(1)
@@ -12,23 +9,21 @@ print("Always Remember Python Is Case Sensitive!")
 time.sleep(1)
 print("Here We Go!")
 time.sleep(1)
-# master
+
 print("Hello World!")
 time.sleep(1)
 print("A Quick Tip!")
 time.sleep(1)
 print(
-    "make sure to use the same type of quotes(quotation marks or apostrophes)at the end that you used at the start"
-)
+    "Make sure to use the same type of quotes (quotation marks or apostrophes) at the end that you used at the start."")
 
-# in c -> printf("Hello World!");
-# in java -> System.out.println("Hello World!");
-# in c++ -> cout << "Hello World";
-# master
-# in javascript - > console.log("Hello World");
-
-# in javascript - > console.log("Hello World") or document.write("Hello World!")
-time.sleep(2)
+time.sleep(1)
 print("All The Best!")
+
+# Hello World in different languages:
+# C -> printf("Hello World!");
+# Java -> System.out.println("Hello World!");
+# C++ -> cout << "Hello World!";
+# JavaScript -> console.log("Hello World!") or document.write("Hello World!")
+
 # Adios!
-# master
